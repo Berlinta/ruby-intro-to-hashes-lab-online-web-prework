775de5a9ab2cc10793a35fe489dbbe69ebdfaaf9
Nash Brancic <nashfft@gmail.com>
2019-02-02 04:45:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,48 +1,47 @@
-def new_hash
-  new_hash ={}
-
+new_hash = {}
 end
 
-def actor
-  {:name => "Dwayne The Rock Johnson"}
 
+ def actor
+ 	actor = {:name => 'Dwayne The Rock Johnson'}
 end
 
-def monopoly
-	monopoly = {
-	  :railroads =>{}
-	}
-
-end
-
-def monopoly_with_second_tier
-		def monopoly
-	monopoly = {}		monopoly = {
-
+ def monopoly
+	monopoly = {}
  		:railroads => {}
-	}
-end	end
-	
+}
 end
 
-def monopoly_with_third_tier
-		monopoly = {
-    railroads: {
-      pieces: 4, 
-      names: {
-        reading_railroad: {}, 
-        pennsylvania_railroad: {}, 
-        b_and_o_railroad: {}, 
-        shortline: {}
-      }, 
-      rent_in_dollars: {
-        one_piece_owned: 25, 
-        two_pieces_owned: 50, 
-        three_pieces_owned: 100, 
-        four_pieces_owned: 200
-      }
-    }
-  }	
+
+ def monopoly_with_second_tier	
+   monopoly = {
+ 		:railroads => {
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {}
+		}
+	}
+end
+
+
+ def monopoly_with_third_tier
+monopoly = {
+ 		:railroads => {
+			:pieces => 4,
+			:names => {
+				:reading_railroad => {},
+				:pennsylvania_railroad => {},
+				:b_and_o_railroad => {},
+				:shortline => {}
+			},
+			:rent_in_dollars => {
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
+			}
+		}
+	}
 end
 
 def monopoly_with_fourth_tier
